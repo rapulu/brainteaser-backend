f5509438cc5171509537b9b08017bfdccc69fdae
fairtraxx <fairtraxx@protonmail.com>
2020-07-09 14:18:42 +0200

    Swagger Documentation

--- quizzes/settings.py
+++ quizzes/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'game.apps.GameConfig'
+    'game.apps.GameConfig',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- quizzes/urls.py
+++ quizzes/urls.py
@@ -14,10 +14,26 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 from django.urls import path, include
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="GAME API",
+      default_version='v1',
+      description="Test Endpoints here",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('game.urls')),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
