bbb00b8cdf4a676c8fd07cda45c70cf07e07c097
poojithamiryala <poojithamiryala@gmail.com>
2020-07-10 15:06:39 +0530

    user dashboard

--- game/urls.py
+++ game/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
 from game.views import create_a_game_code, check_if_game_code_isValid, end_game, update_score_usergame, \
-    get_leader_board_game_code, get_leader_board,update_score_count_usergame,get_all_category,create_category,create_question
+    get_leader_board_game_code, get_leader_board, update_score_count_usergame, get_all_category, create_category, \
+    create_question, get_user_dasboard
 
 urlpatterns = [
     path('game', create_a_game_code),
@@ -13,6 +14,7 @@ urlpatterns = [
     path('game/leaderboard', get_leader_board),
     path('game/category', get_all_category),
     path('game/create/category', create_category),
-    path('game/create/question', create_question)
+    path('game/create/question', create_question),
+    path('game/user/dashboard', get_user_dasboard)
 ]
 
