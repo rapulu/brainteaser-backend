bb923fb782f3409e3d6096959ff7554002a8ede4
fairtraxx <fairtraxx@protonmail.com>
2020-07-10 09:43:20 +0200

    create/display category, create a new question

--- quizzes/urls.py
+++ quizzes/urls.py
@@ -1,18 +1,4 @@
-"""quizzes URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
@@ -21,9 +7,9 @@ from django.urls import path, include
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Brain Teaser API",
+      title="Game API",
       default_version='v1',
-      description="Test Endpoints here",
+      description="Test description",
       terms_of_service="https://www.google.com/policies/terms/",
       contact=openapi.Contact(email="contact@snippets.local"),
       license=openapi.License(name="BSD License"),
